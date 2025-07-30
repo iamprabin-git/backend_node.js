a10323bc53900af147a5e9306a436aac99f6343a
Prabin Dangol <dprabin23@gmail.com>
2025-07-30 20:31:48 +0545

    first commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
-
 import authRoutes from "./routes/authRoute.js";
 import connectCloudinary from "./config/cloudinary.js";
 import connectDB from "./config/database.js";
@@ -12,8 +10,8 @@ import orderRoutes from "./routes/orderRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import userRoutes from "./routes/userRoute.js";
 import viewRoutes from "./routes/viewRoute.js";
+import config from "./config/index.js";
 
-dotenv.config();
 
 const app = express();
 
@@ -33,15 +31,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "hbs");
 
-const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    name: "nodejs-20250302",
-    status: "OK",
-    version: "1.1.0",
-    url: "https://node-20250302.vercel.app",
-    port: port,
+    port: config.port, 
+    name: config.name,
+    version: config.version,
   });
 });
 
@@ -51,6 +46,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/page", viewRoutes);
 
-app.listen(port, () => {
-  console.log(`Server started at port ${port}...`);
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}.....`);
 });
---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export default {
+    port: process.env.PORT || 5000,
+    name: process.env.APP_NAME || 'ecommerce_backend',
+    version: process.env.APP_VERSION || '0.1.0',
+    mongoUrl: process.env.MONGO_URI || "",
+};
